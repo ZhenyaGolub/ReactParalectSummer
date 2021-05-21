335484bab9e137950c8d9e0baea35e3ac74c7e2c
Zhenya <golubokyo@gmail.com>
2021-05-21 12:31:46 +0300

    the loader was created

--- src/App.js
+++ src/App.js
@@ -6,17 +6,30 @@ import './styles/main.scss';
 import Header from './components/Header';
 import Empty from './components/Empty';
 import User from './components/User';
-
+import Loader from './components/Loader';
 
 const App = () => {
 
-    const user = useSelector(({user}) => user);
+    const { user, isLoader} = useSelector((state) => state);
+
+    const render = () => {
+        if(isLoader){
+            return (
+                <Loader/>
+            )
+        } else{
+            return(
+                user ? <User user={user}/> : <Empty/>
+            )
+        }
+    }
+    
 
     return (
         <div>
             <Header/>
             {
-                user ? <User user={user}/> : <Empty/>
+                render()
             }
             
         </div>
--- src/components/Repositories.jsx
+++ src/components/Repositories.jsx
@@ -5,25 +5,35 @@ const Repositories = ({ repositories }) => {
 
     const { totalAmount, list } = repositories;
 
-    return (
-        <div className="main__user-repositories">
-            <h1 className="main__user-repositories-title">Repositories ({totalAmount})</h1>
-            <div className="main__user-repositories-list">
-                {
-                    list.map(repository => {
-                        return (
-                            <div key={repository.id} className="main__user-repositories-item">
-                                <a href={repository.link} className="main__user-repositories-name">{repository.name}</a>
-                                <p className="main__user-repositories-description">{repository.description}</p>
-                            </div>
-                        )
-                    })
-                }
-                
-            </div>
-            <Pagination/>
+    if(!list.length){
+        return (
+            <div className="main__user-repositories empty-repositories">
+            <div className="empty-repositories-content">
+                    <i className="fas fa-archive"></i>
+                    <p>Repository list is empty</p>
+                </div>
         </div>
-    )
+        )
+    } else {
+        return (
+            <div className="main__user-repositories">
+                <h1 className="main__user-repositories-title">Repositories ({totalAmount})</h1>
+                <div className="main__user-repositories-list">
+                    {
+                        list.map(repository => {
+                            return (
+                                <div key={repository.id} className="main__user-repositories-item">
+                                    <a href={repository.link} className="main__user-repositories-name">{repository.name}</a>
+                                    <p className="main__user-repositories-description">{repository.description}</p>
+                                </div>
+                            )
+                        })
+                    } 
+                </div>
+                <Pagination repositoriesAmount={totalAmount}/>
+            </div>
+        )
+    }
 }
 
 export default Repositories
